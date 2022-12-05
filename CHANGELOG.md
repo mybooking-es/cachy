aacfbaeb2bcb135abbdc5879f26604db8593ae8b
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2022-12-05 23:23:06 +0100

    fixed moneta integration

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## [0.5.0] 2022-12-05
+
+### Added
+
+- Moneta Wrapper delete key
+
+### Fixed
+
+- Moneta Wrapper transform expires_in: into expires: in order to work with expires 
--- lib/cachy/moneta_wrapper.rb
+++ lib/cachy/moneta_wrapper.rb
@@ -3,6 +3,18 @@ require 'cachy/wrapper'
 # Wrapper for Moneta http://github.com/wycats/moneta/tree/master
 class Cachy::MonetaWrapper < Cachy::Wrapper
   def write(key, result, options={})
+    # Transform :expires_in into :expires
+    if options.has_key?(:expires_in)
+      options.store(:expires, options[:expires_in].to_i)
+      options.delete(:expires_in)
+    end
+    ::Yito::Logger.instance.logger.debug  "MONETA-WRAPPER:: write #{key} options: #{options.inspect}"
     @wrapped.store(key, result, options)
   end
+
+  def delete(key, options={})
+    ::Yito::Logger.instance.logger.debug  "MONETA-WRAPPER:: delete #{key} options: #{options.inspect}"
+    @wrapped.delete(key)
+  end
+
 end
